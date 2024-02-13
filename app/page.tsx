8223c9183f42318dada753161e0340506aa566fc
Szymon Szmydt <szymonszmydt@gmail.com>
2024-02-13 19:14:43 +0100

    A form with styles has been created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import ExchangeForm from "./components/form/ExchangeForm";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <input type='text' placeholder='Kurs' />
+      <ExchangeForm />
     </main>
   );
 }
